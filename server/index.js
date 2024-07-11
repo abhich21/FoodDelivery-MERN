d0880a5ee374cac7a7d982ce193805194ef3045e
Abhishek Choudhary <abhich2109@gmail.com>
2024-07-11 06:11:27 +0530

    added error handler

--- server/index.js
+++ server/index.js
@@ -9,6 +9,19 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); // for form data
 
+
+// error handler
+app.use((err, req, res, next) => {
+    const status = err.status || 500;
+    const message = err.message || "Something went wrong";
+    return res.status(status).json({
+      success: false,
+      status,
+      message,
+    });
+  });
+  
+
 app.get("/", async (req, res) => {
     res.status(200).json({
       message: "Hello from Abhishek",
